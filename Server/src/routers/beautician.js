0c500319971ada2ec4b48f8ddc9481717ccf9f19
Pavan S <54991142+pavanshetty-in@users.noreply.github.com>
2021-08-21 02:37:57 +0530

    beautician page done

--- Server/src/routers/beautician.js
+++ Server/src/routers/beautician.js
@@ -15,38 +15,40 @@ const bcrypt = require("bcryptjs");
 beauticianRouter.get("/beauticianLogin", (req, res) => {
     res.render("beauticianLogin");
 });
-beauticianRouter.get("/beauticianpage", beauticianAuthenticate, async (req, res) => {
-    res.render("beauticianpage");
-});
+// beauticianRouter.get("/beauticianpage", beauticianAuthenticate, async (req, res) => {
+//     res.render("beauticianpage");
+// });
 beauticianRouter.get("/beautician/appointment", beauticianAuthenticate, async (req, res) => {
     const _id = req.rootBeautician._id;
+    const name = req.rootBeautician.name;
     try {
         const appointments = await Appointment.find({ assign: _id, status: "pending" });
         if (!appointments) {
             throw new Error("Appointments not found");
         }
-        const appointmentsSalon = await AppointmentSalon.find({ assign: _id, status: "pending" });
-        if (!appointmentsSalon) {
-            throw new Error("Appointments not found");
-        }
+        // const appointmentsSalon = await AppointmentSalon.find({ assign: _id, status: "pending" });
+        // if (!appointmentsSalon) {
+        //     throw new Error("Appointments not found");
+        // }
         console.log(appointments);
-        console.log(appointmentsSalon);
-        console.log(_id);
-        // res.render("appointment", { service: appointment });
+        // console.log(appointmentsSalon);
+        console.log(name);
+        res.render("beauticianpage", { appointments: appointments,name:name });
     } catch (err) {
         res.status(401).send("Unauthorized:No token provided");
         console.log(err);
     }
 });
 //Edit Appointment
-beauticianRouter.put("/editAppointment/:id", beauticianAuthenticate, async (req, res) => {
+beauticianRouter.put("/beautician/editAppointment/:id", beauticianAuthenticate, async (req, res) => {
     try {
         const _id = req.params.id;
         const appointment = await Appointment.findByIdAndUpdate(_id, req.body, {
             new: true,
         });
-        // res.redirect("/listAppointments");
+       
         console.log(appointment);
+         res.redirect("/beautician/appointment");
     } catch (err) {
         res.status(500).send(err);
     }
@@ -83,7 +85,7 @@ beauticianRouter.post("/beauticianlogin", async (req, res) => {
                     expires: new Date(Date.now + 25892000000),
                     httpOnly: true,
                 });
-                return res.status(201).render("beauticianpage");
+                return res.redirect("/beautician/appointment");
             }
         } else {
             return res.status(400).json({ error: "Invalid Credentials!" });
